08ad78153300147a90c213ced015bc72236a2462
psi77 <simon@smith77.co.uk>
2021-12-05 11:34:31 +0000

    Day 1 part 2

--- aoc2021/day1.py
+++ aoc2021/day1.py
@@ -12,5 +12,15 @@ def part1():
     print(f"{count} increases")
 
 
+def part2():
+    count, _, _, _ = reduce(
+        lambda c, n: (c[0] + (1 if int(n) > c[3] else 0), int(n), c[1], c[2]),
+        load(day=1),
+        (0, 1000000, 100000, 100000),
+    )
+    print(f"{count} increases")
+
+
 if __name__ == "__main__":
     part1()
+    part2()
