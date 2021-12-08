87b65c3eb5a8bab8d0e17773a475bfe744655d0d
psi77 <simon@smith77.co.uk>
2021-12-08 19:41:01 +0000

    Day 2 part 2

--- aoc2021/day2.py
+++ aoc2021/day2.py
@@ -26,3 +26,32 @@ print(
         )
     )[0]
 )
+
+print(
+    list(
+        map(
+            lambda l: l[0] * l[1],
+            [
+                reduce(
+                    lambda p1, p2: (
+                        p1[0] + p2[0],
+                        p1[1] + (p2[0] * p1[2]),
+                        p1[2] + p2[1],
+                    ),
+                    map(
+                        lambda a: (
+                            int(a[1]) if a[0] == "forward" else 0,
+                            int(a[1])
+                            if a[0] == "down"
+                            else -int(a[1])
+                            if a[0] == "up"
+                            else 0,
+                        ),
+                        [d.split(" ") for d in load(day=2, test=False)],
+                    ),
+                    (0, 0, 0),
+                )
+            ],
+        )
+    )[0]
+)
